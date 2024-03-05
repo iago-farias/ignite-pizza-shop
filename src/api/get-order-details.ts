1457faf2bb89b61133b46976d9c80f8126c408dc
Iago Farias <iago@insidethebox.com.br>
2024-03-04 23:20:53 -0300

    feat: Implement order details modal api integration.

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,33 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrderDetailsParams {
+  orderId: string
+}
+
+export interface GetOrderDetailsResponse {
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  id: string
+  createdAt: Date
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function getOrderDetails({
+  orderId,
+}: GetOrderDetailsParams): Promise<GetOrderDetailsResponse> {
+  const response = await api.get(`/orders/${orderId}`)
+
+  return response.data
+}
