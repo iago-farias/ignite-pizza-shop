9d2232f3ad2d3a52832cbb525e0c8e153aee3e1c
iago-farias <iagox38@gmail.com>
2024-09-29 13:59:34 -0300

    fix: Fix orders.e2e-spec.ts paginate orders e2e test.

--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -17,41 +17,41 @@ test('paginate orders', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Próxima página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 11', exact: true }),
   ).toBeVisible()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 20', exact: true }),
   ).toBeVisible()
 
   await page.getByRole('button', { name: 'Última página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 51', exact: true }),
   ).toBeVisible()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 60', exact: true }),
   ).toBeVisible()
 
   await page.getByRole('button', { name: 'Página anterior' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 41', exact: true }),
   ).toBeVisible()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 50', exact: true }),
   ).toBeVisible()
 
   await page.getByRole('button', { name: 'Primeira página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 1', exact: true }),
   ).toBeVisible()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 10', exact: true }),
   ).toBeVisible()
 })
